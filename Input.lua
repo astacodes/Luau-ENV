9695fbb35fc85e541f3a691522312cb1bad15a53
astacodes <127895682+astacodes@users.noreply.github.com>
2024-10-14 11:46:41 -0400

    Update Input.lua

--- Input.lua
+++ Input.lua
@@ -19,50 +19,50 @@ getgenv().mouse1click = function(x, y)
     x = x or 0
     y = y or 0
 
-    VirtualInputManager:SendMouseButtonEvent(x, y, 0, true, _game, false)
+    VirtualInputManager:SendMouseButtonEvent(x, y, 0, true, game, false)
     task.wait()
-    VirtualInputManager:SendMouseButtonEvent(x, y, 0, false, _game, false)
+    VirtualInputManager:SendMouseButtonEvent(x, y, 0, false, game, false)
 end
 
 getgenv().mouse1press = function(x, y)
     x = x or 0
     y = y or 0
 
-    VirtualInputManager:SendMouseButtonEvent(x, y, 0, true, _game, false)
+    VirtualInputManager:SendMouseButtonEvent(x, y, 0, true, game, false)
 end
 
 getgenv().mouse1release = function(x, y)
     x = x or 0
     y = y or 0
 
-    VirtualInputManager:SendMouseButtonEvent(x, y, 0, false, _game, false)
+    VirtualInputManager:SendMouseButtonEvent(x, y, 0, false, game, false)
 end
 
 getgenv().mouse2click = function(x, y)
     x = x or 0
     y = y or 0
 
-    VirtualInputManager:SendMouseButtonEvent(x, y, 1, true, _game, false)
+    VirtualInputManager:SendMouseButtonEvent(x, y, 1, true, game, false)
     task.wait()
-    VirtualInputManager:SendMouseButtonEvent(x, y, 1, false, _game, false)
+    VirtualInputManager:SendMouseButtonEvent(x, y, 1, false, game, false)
 end
 
 getgenv().mouse2press = function(x, y)
     x = x or 0
     y = y or 0
 
-    VirtualInputManager:SendMouseButtonEvent(x, y, 1, true, _game, false)
+    VirtualInputManager:SendMouseButtonEvent(x, y, 1, true, game, false)
 end
 
 getgenv().mouse2release = function(x, y)
     x = x or 0
     y = y or 0
 
-    VirtualInputManager:SendMouseButtonEvent(x, y, 1, false, _game, false)
+    VirtualInputManager:SendMouseButtonEvent(x, y, 1, false, game, false)
 end
 
 getgenv().mousescroll = function(x, y, z)
-    VirtualInputManager:SendMouseWheelEvent(x or 0, y or 0, z or false, _game)
+    VirtualInputManager:SendMouseWheelEvent(x or 0, y or 0, z or false, game)
 end
 
 getgenv().mousemoverel = function(x, y)
@@ -73,12 +73,12 @@ getgenv().mousemoverel = function(x, y)
     local x = vpSize.X * x
     local y = vpSize.Y * y
 
-    VirtualInputManager:SendMouseMoveEvent(x, y, _game)
+    VirtualInputManager:SendMouseMoveEvent(x, y, game)
 end
 
 getgenv().mousemoveabs = function(x, y)
     x = x or 0
     y = y or 0
 
-    VirtualInputManager:SendMouseMoveEvent(x, y, _game)
+    VirtualInputManager:SendMouseMoveEvent(x, y, game)
 end
